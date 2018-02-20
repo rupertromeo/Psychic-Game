412353506aedf32115a621a19044d0ca09925ba1
Manuel Romo <rupertromeo86@gmail.com>
2018-02-19 20:36:34 -0700

    Updated reset for gameplay

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,42 @@
 
-//These are the known variables
-	var possibleLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-	var correctGuess = 0;
-	var incorrectGuess = 0;
-	var guessCount = 10;
-	var wrongGuess = [];
+//This section contains our known variables within the game
+	//These are all of the possible letters to be guessed
+	const possibleLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+	//This is an empty array used to hold the letters that have been guessed
+	let wrongGuess = [];
+
+	//This is our random letter to be guessed variable
+	let appChoice = null;
+
+	//This is our win counter
+	let correctGuess = 0;
+
+	//This is our loss counter
+	let incorrectGuess = 0;
+
+	//This is our guesses remaining counter
+	let guessCount = 9;
 
 	//This is the randomly generated letter to guess
-		var appChoice = possibleLetters[Math.floor(Math.random() * possibleLetters.length)];
+	const letterToGuess = function() {
+		appChoice = possibleLetters[Math.floor(Math.random() * possibleLetters.length)];
 			console.log(appChoice);
-	
+	};
+
+	//This is our game reset function
+	const reset = function() {
+		guessCount = 9;
+		wrongGuess = [];
+		letterToGuess();
+	};	
 
 
 //This is the gameplay function that generates the random letter and checks for user input 	
 	document.onkeyup = function(event) {
 		
 	//This is the letter entered by the user
-		var userGuess = event.key;
+		const userGuess = event.key;
 			console.log(userGuess);
 
 
@@ -26,6 +46,7 @@
 			correctGuess++;
 			//This writes points to the winners span id
 			document.getElementById("winners").innerHTML = correctGuess;
+			reset();
 		}
 
 		else {
@@ -42,9 +63,10 @@
 		if (guessCount === 0) {
 			incorrectGuess++;
 			document.getElementById("losers").innerHTML = incorrectGuess;
+			reset();
 		}
 		
-	}
+	};
 	
 
 	
